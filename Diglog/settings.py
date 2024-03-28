17ccf207018c7deeef0c81f9e30a4ee061e0bc0d
Mohammad Mousapour <mohammadmoosapoor4@gmail.com>
2024-03-28 16:48:12 +0330

    User authentication

--- Diglog/settings.py
+++ Diglog/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'members',
+
 ]
 
 MIDDLEWARE = [
@@ -125,3 +127,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'home-url'
+LOGOUT_REDIRECT_URL = 'home-url'
