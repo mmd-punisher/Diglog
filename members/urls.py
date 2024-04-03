56f1bcdbd20953743a5fdc4e598bb0f34a3e4e21
Mohammad Mousapour <mohammadmoosapoor4@gmail.com>
2024-04-04 02:11:28 +0330

    Edit profile

--- members/urls.py
+++ members/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UserRegisterView
+from .views import UserRegisterView, UserEditView
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register-url'),
+    path('edit-profile/', UserEditView.as_view(), name='edit-profile-url'),
 
 ]
--- members/views.py
+++ members/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserChangeForm
 from django.urls import reverse_lazy
-from .forms import RegisterForm
+from .forms import RegisterForm, EditProfileForm
 
 
 class UserRegisterView(generic.CreateView):
     form_class = RegisterForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
+
+
+class UserEditView(generic.UpdateView):
+    form_class = EditProfileForm
+    template_name = 'registration/edit_profile.html'
+    success_url = reverse_lazy('home-url')
+
+    def get_object(self):
+        return self.request.user
