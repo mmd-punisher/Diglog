1eb17077f0a65cd753016333024bf23478cfffbf
Mohammad Mousapour <mohammadmoosapoor4@gmail.com>
2024-05-01 17:11:46 +0330

    Main Page Template

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home-url'),
+    path('', views.index, name='index'),
+    # path('', views.HomeView.as_view(), name='home-url'),
     path('post/<slug:slug>/', views.PostDetailView.as_view(), name='post-detail-url'),
     path('create/', views.PostCreateView.as_view(), name='create-post-url'),
     path('edit/<slug:slug>/', views.PostUpdateView.as_view(), name='edit-post-url'),
