73c339ca6520eef963b2e84020ba83d04333c89f
Mohammad Mousapour <mohammadmoosapoor4@gmail.com>
2024-05-02 13:35:25 +0330

    Load Category In All Templates

--- landing/views.py
+++ landing/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 
+from blog.models import Category
+
 
 def index(request):
     return render(request, 'landing/index_page.html')
 
 
 def header_component(request):
-    return render(request, 'landing/header_partial.html')
+    categories_menu = Category.objects.all()
+    all_cats = []
+    for cat in categories_menu:
+        all_cats.append(cat.name)
+    context = {'categories_menu': all_cats}
+    return render(request, 'landing/header_partial.html', context)
 
 
 def footer_component(request):
